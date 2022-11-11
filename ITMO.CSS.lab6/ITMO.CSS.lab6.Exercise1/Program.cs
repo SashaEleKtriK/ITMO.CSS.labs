8dedd49d9df1fb18c07c83e32702a4da83ed7731
HP <HP@DESKTOP-GQ9FHG9>
2022-11-11 12:06:03 +0300

    Добавьте файлы проекта.

---
+++ ITMO.CSS.lab6/ITMO.CSS.lab6.Exercise1/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.CSS.lab6.Exercise1
+{
+
+    enum AccountType
+    {
+        Checking,
+        Deposit
+    }
+
+    //struct BankAccount
+    //{
+    //    public long accNo;
+    //    public decimal accBal;
+    //    public AccountType accType;
+    //}
+    class BankAccount
+    {
+        public void Populate(long number, decimal balance)
+        {
+            accNo = number;
+            accBal = balance;
+            accType = AccountType.Checking;
+        }
+        public long Number()
+        {
+            return accNo;
+        }
+
+        public decimal Balance()
+        {
+            return accBal;
+        }
+
+        public string Type()
+        {
+            return accType.ToString();
+        }
+
+
+
+        private long accNo;
+        private decimal accBal;
+        private AccountType accType;
+    }
+    class CreateAcc    
+    {
+
+        static void Main()
+        {
+            BankAccount berts = NewBankAccount();
+            Write(berts);
+
+            BankAccount freds = NewBankAccount();
+            Write(freds);
+        }
+
+        static BankAccount NewBankAccount()
+        {
+            //BankAccount created;
+            BankAccount created = new BankAccount();
+
+            Console.Write("Enter the account number   : ");
+            long number = long.Parse(Console.ReadLine());
+
+            Console.Write("Enter the account balance! : ");
+            decimal balance = decimal.Parse(Console.ReadLine());
+
+            //created.accNo = number;
+            //created.accBal = balance;
+            //created.accType = AccountType.Checking;
+            created.Populate(number, balance);
+
+
+            return created;
+        }
+
+        //static void Write(BankAccount toWrite)
+        //{
+        //    Console.WriteLine("Account number is {0}", toWrite.accNo);
+        //    Console.WriteLine("Account balance is {0}", toWrite.accBal);
+        //    Console.WriteLine("Account type is {0}", toWrite.accType.ToString());
+        //}
+        static void Write(BankAccount toWrite)
+        {
+            Console.WriteLine("Account number is {0}", toWrite.Number());
+            Console.WriteLine("Account balance is {0}", toWrite.Balance());
+            Console.WriteLine("Account type is {0}", toWrite.Type());
+        }
+
+    }
+}
---
+++ ITMO.CSS.lab6/ITMO.CSS.lab6.Exercise1/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("ITMO.CSS.lab6.Exercise1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hewlett-Packard")]
+[assembly: AssemblyProduct("ITMO.CSS.lab6.Exercise1")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("238db8c5-aeda-4ab1-a0d6-38330f92ac3c")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
